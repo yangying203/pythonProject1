73f20d2daf9ebafd282081307e8362df562acde4
yy <2444532593@qq.com>
2022-06-24 23:17:22 +0800

    周五学习

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print('kkk')
+name = 'yy'
+
+
+def fun1(fun):
+    print(name)
+
+    def fun2():
+        fun()
+        name = 'ii'
+        print(name)
+        print('fun2')
+    return fun2
+
+
+@fun1
+def decorete():
+    print('hhhh')
+decorete()
+
